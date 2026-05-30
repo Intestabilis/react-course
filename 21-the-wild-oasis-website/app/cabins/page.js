18a7f07c95c58ffbf365e64c8c9d9e55e0333e28
Max <Maxym.Ptsk@gmail.com>
2026-05-30 22:08:52 +0300

    Added Section 32

---
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -0,0 +1,24 @@
+import Counter from "../components/Counter";
+
+// This react function (component) can be async
+export default async function Page() {
+  // Fetching data directly in the server component
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  console.log(data);
+  return (
+    <div>
+      <h1>Cabins page</h1>
+
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <Counter users={data} />
+    </div>
+  );
+}
---
+++ 21-the-wild-oasis-website/app/layout.js
@@ -0,0 +1,19 @@
+import Logo from "./components/Logo";
+import Navigation from "./components/Navigation";
+
+export const metadata = { title: "The Wild Oasis" };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <Logo />
+        </header>
+        <Navigation />
+        <main>{children}</main>
+        <footer>Copyright by the wild oasis</footer>
+      </body>
+    </html>
+  );
+}
